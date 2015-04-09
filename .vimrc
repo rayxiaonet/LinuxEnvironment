f05af4b8507015ee5eff1e56a91e197d87fc79f1
Ray Xiao <rxiao@amgen.com>
2015-04-09 14:30:48 -0700

    add vim

---
+++ .vimrc
@@ -0,0 +1,5 @@
+execute pathogen#infect()
+syntax on
+filetype plugin indent on
+set tabstop=4 softtabstop=0 expandtab shiftwidth=4 smarttab
+map <C-n> :NERDTreeToggle<CR>
--- setuplinks.sh
+++ setuplinks.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-ln -fv RAY.screenrc $HOME/.screenrc
+ln -sfv $HOME/.screenrc $HOME/LinuxEnvironment/.screenrc
+ln -sfv $HOME/.vimrc $HOME/LinuxEnvironment/.vimrc
+ln -sfv $HOME/.vim/ $HOME/LinuxEnvironment/.vim/
